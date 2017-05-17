a73539efcea79d1f90186fd9c28e6016f48dcb91
Andrzej Wódkiewicz <akwodkiewicz@gmail.com>
2017-05-17 15:34:59 +0200

    Added 'user' and 'watched_pages' field, got rid of unnecessary decorators and resolved circular dependency problem

--- app/models.py
+++ app/models.py
@@ -5,13 +5,6 @@ from . import app, mongo
 from .views import list_users_page
 
 
-class User(graphene.ObjectType):
-    _id = graphene.ID()
-    username = graphene.String()
-    password = graphene.String()
-    email = graphene.String()
-
-
 class WatchedPage(graphene.ObjectType):
     _id = graphene.ID()
     owner_name = graphene.String()
@@ -19,45 +12,62 @@ class WatchedPage(graphene.ObjectType):
     url = graphene.String()
     authentication = graphene.String()
     interval = graphene.Int()
+    user = graphene.Field(lambda: User)
+
+    def resolve_user(self, args, context, info):
+        query_dict = {}
+        if self.owner_name is not None:
+            query_dict['username'] = self.owner_name
+        result = mongo.db.users.find_one(query_dict)
+        if result is not None:
+            return User(**result)
+        return None
+
+
+class User(graphene.ObjectType):
+    _id = graphene.ID()
+    username = graphene.String()
+    password = graphene.String()
+    email = graphene.String()
+    watched_pages = graphene.List(lambda: WatchedPage)
+
+    def resolve_watched_pages(self, args, context, info):
+        result = list(mongo.db.watched_pages.find({"owner_name":self.username}))
+        if result is not None:
+            return [WatchedPage(**kwargs) for kwargs in result]
+        return None
 
 
 class Query(graphene.ObjectType):
-    user = graphene.List(User,
+    users = graphene.List(User,
                         _id=graphene.ID(),
                         username=graphene.String()
                         )
-    watched_page = graphene.List(WatchedPage,
+    watched_pages = graphene.List(WatchedPage,
                         _id=graphene.ID(),
-                        owner_name=graphene.String(),
                         page_name=graphene.String(),
                         url=graphene.String()
                         )
 
-    @graphene.resolve_only_args
-    def resolve_user(self, username=None):
+    def resolve_users(self, args, context, info):
         query_dict = {}
-        if username is not None:
-            query_dict['username'] = username
+        if args.get('username') is not None:
+            query_dict['username'] = args['username']
         result = list(mongo.db.users.find(query_dict))
         if result is not None:
-            # EUREKA!@#! -- time needed to realise that dicts are unpackable
-            # and we don't need a specialized mongodb-graphene module to
-            # automagically change dicts into instances of classes: 1h45min
-            return [User(**kwargs) for kwargs in result] # BTW python magic
+            return [User(**kwargs) for kwargs in result]
         return None
 
-    @graphene.resolve_only_args
-    def resolve_watched_page(self, _id=None, owner_name=None, page_name=None, url=None):
+    def resolve_watched_pages(self, args, context, info):
         query_dict = {}
-        if _id is not None:
+        if args.get('_id') is not None:
             query_dict['_id'] = _id
-        if owner_name is not None:
+        if args.get('owner_name') is not None:
             query_dict['owner_name'] = owner_name
-        if page_name is not None:
+        if args.get('page_name') is not None:
             query_dict['page_name'] = page_name
-        if url is not None:
+        if args.get('url') is not None:
             query_dict['url'] = url
-
         result = list(mongo.db.watched_pages.find(query_dict))
         if result is not None:
             return [WatchedPage(**kwargs) for kwargs in result]
